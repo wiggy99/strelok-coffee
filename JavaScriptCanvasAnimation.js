9d3bbd790205a20fa04745a4f1c2ee24b7946069
wiggy99 <57639380+wiggy99@users.noreply.github.com>
2019-11-19 08:38:30 +1300

    js

---
+++ JavaScriptCanvasAnimation.js
@@ -0,0 +1,21 @@
+﻿// set up initial variables
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(100, 0);
+ctx.lineTo(100, 200);
+ctx.stroke();
+
+function drawCircle(x) {
+    ctx.beginPath();
+    ctx.arc(x, 100, 10, 0, 2 * Math.PI);
+    ctx.fillStyle = "red";
+    ctx.fill();
+}
+var x = 0;
+setInterval(function () {
+    ctx.clearRect(0, 0, 200, 200);
+    drawCircle(x % 200);
+    x++;
+}, 25);
